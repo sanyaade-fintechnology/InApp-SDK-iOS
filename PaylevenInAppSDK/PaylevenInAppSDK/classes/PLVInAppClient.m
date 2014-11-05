8fd38439cbdd22916e0c95d242922f6083a5246c
Thomas Ploentzke <ploenne@ploenne.net>
2014-11-05 17:20:12 +0100

    add ValidationTests

--- PaylevenInAppSDK/PaylevenInAppSDK/classes/PLVInAppClient.m
+++ PaylevenInAppSDK/PaylevenInAppSDK/classes/PLVInAppClient.m
@@ -10,12 +10,14 @@
 #import "SingletonHelper.h"
 #import "PLVInAppClient.h"
 #import "PLVInAppAPIClient.h"
+#import "PLVInAppClientTypes.h"
 #import "PLVInAppSDKConstants.h"
 
 @interface PLVInAppClient ()
 
 @property (strong) NSString* apiKey;
 @property (strong) NSString* bundleID;
+@property (strong) NSError* lastError;
 
 /** Serial operation queue. */
 @property (nonatomic, strong) NSOperationQueue *queue;
@@ -38,7 +40,7 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(PLVInAppClient)
         
         _bundleID = [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleIdentifierKey];
         
-        SDLog(@"PLVInAppClient created 123456 BundleID: %@",_bundleID);
+        SDLog(@"PLVInAppClient created with BundleID: %@",_bundleID);
         
         _queue = [[NSOperationQueue alloc] init];
         _queue.maxConcurrentOperationCount = 1;
@@ -55,9 +57,14 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(PLVInAppClient)
     
     [self.inAppAPIClient registerWithAPIKey:apiKey andBundleID:self.bundleID];
     
+    self.apiKey = apiKey;
+    
 }
 
 - (void) getUserToken:(NSString*)emailAddress withCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
+    
+    // run validation check
+    if (![self checkEmailAddress:emailAddress andCompletion:completionHandler]) { return; }
     
     [self.inAppAPIClient userTokenForEmail:emailAddress withCompletion:completionHandler];
     
@@ -65,19 +72,236 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(PLVInAppClient)
 
 - (void) addPaymentInstruments:(NSArray*)piArray forUserToken:(PLVInAppUserToken*)userToken withCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
     
+    // run validation check
+    if (![self checkUserToken:userToken withPIAsArray:piArray andCompletion:completionHandler]) { return; }
+    
     [self.inAppAPIClient addPaymentInstruments:piArray forUserToken:userToken withCompletion:completionHandler];
 }
 
 - (void) listPaymentInstrumentsForUserToken:(PLVInAppUserToken*)userToken withCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
     
+    // run validation check
+    if (![self checkUserToken:userToken andCompletion:completionHandler]) { return; }
+    
     [self.inAppAPIClient listPaymentInstrumentsForUserToken:userToken withCompletion:completionHandler];
 }
 
-- (void) setPaymentInstrumentsOrder:(NSOrderedSet*)piOrder forUserToken:(PLVInAppUserToken*)userToken withCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
+- (void) setPaymentInstrumentsOrder:(NSOrderedSet*)piOrderedSet forUserToken:(PLVInAppUserToken*)userToken withCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
     
-    [self.inAppAPIClient setPaymentInstrumentsOrder:piOrder forUserToken:userToken withCompletion:completionHandler];
+    // run validation check
+    if (![self checkUserToken:userToken withPIAsOrderedSet:piOrderedSet andCompletion:completionHandler]) { return; }
+    
+    [self.inAppAPIClient setPaymentInstrumentsOrder:piOrderedSet forUserToken:userToken withCompletion:completionHandler];
     
 }
 
+
+/**
+ *  checkUserToken withPIAsOrderedSet
+ *
+ *  Before runing BE communication
+ *  Checks for valid API Key and completion Handler
+ *  @param piOrderedSet      piOrderedSet to check
+ *  @param completionHandler completionHandler to check
+ *  @param userToken         userToken to check
+ *
+ *  @return TRUE for passed Checks, FALSE for failed Checks
+ */
+
+- (BOOL) checkUserToken:(NSString*)userToken withPIAsOrderedSet:(NSOrderedSet*)piOrderedSet andCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
+    
+    if([self checkUserToken:userToken andCompletion:completionHandler]) {
+        
+        if (piOrderedSet != Nil) {
+            
+            if ([piOrderedSet isKindOfClass:[NSOrderedSet class]]) {
+                
+                if(piOrderedSet.count > 0) {
+                    
+                    BOOL allValid = TRUE;
+                    
+                    for (id item in piOrderedSet) {
+                        if (![item isKindOfClass:[PLVPaymentInstrument class]]) {
+                            allValid = FALSE;
+                        }
+                    }
+                    
+                    self.lastError = Nil;
+                    return allValid;
+                }
+            }
+        }
+        
+        NSError* error = [NSError errorWithDomain:kInAppSDKErrorDomain code:ERROR_MISSING_PAYMENTINSTRUMENTS_CODE userInfo:[NSDictionary dictionaryWithObject:ERROR_MISSING_PAYMENTINSTRUMENTS_MESSAGE forKey:NSLocalizedDescriptionKey]];
+        self.lastError = error;
+        
+        completionHandler(nil,error);
+    }
+    
+    return FALSE;
+}
+
+/**
+ *  checkUserToken withPIAsArray
+ *
+ *  Before runing BE communication
+ *  Checks for valid API Key and completion Handler
+ *  @param piArray           piArray to check*
+ *  @param completionHandler completionHandler to check
+ *  @param userToken         userToken to check
+ *
+ *  @return TRUE for passed Checks, FALSE for failed Checks
+ */
+
+- (BOOL) checkUserToken:(NSString*)userToken withPIAsArray:(NSArray*)piArray andCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
+    
+    if([self checkUserToken:userToken andCompletion:completionHandler]) {
+        
+        if (piArray != Nil) {
+            
+            if ([piArray isKindOfClass:[NSArray class]]) {
+                
+                if(piArray.count > 0) {
+                    
+                    BOOL allValid = TRUE;
+                    
+                    for (id item in piArray) {
+                        if (![item isKindOfClass:[PLVPaymentInstrument class]]) {
+                            allValid = FALSE;
+                        }
+                    }
+                    
+                    self.lastError = Nil;
+                    return allValid;
+                }
+            }
+        }
+        
+        NSError* error = [NSError errorWithDomain:kInAppSDKErrorDomain code:ERROR_MISSING_PAYMENTINSTRUMENTS_CODE userInfo:[NSDictionary dictionaryWithObject:ERROR_MISSING_PAYMENTINSTRUMENTS_MESSAGE forKey:NSLocalizedDescriptionKey]];
+        self.lastError = error;
+        
+        completionHandler(nil,error);
+    }
+    
+    return FALSE;
+}
+
+
+
+/**
+ *  checkUserToken
+ *
+ *  Before runing BE communication
+ *  Checks for valid API Key and completion Handler
+ *
+ *  @param completionHandler completionHandler to check
+ *  @param emailAddress      email address
+ *
+ *  @return TRUE for passed Checks, FALSE for failed Checks
+ */
+
+- (BOOL) checkUserToken:(NSString*)userToken andCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
+    
+    if ([self checkAPIKeyWithCompletion:completionHandler]) {
+        
+        if (userToken != Nil) {
+            
+            if ([userToken isKindOfClass:[NSString class]]) {
+                
+                if(userToken.length > 0) {
+                    self.lastError = Nil;
+                    return TRUE;
+                }
+            }
+        }
+        
+        NSError* error = [NSError errorWithDomain:kInAppSDKErrorDomain code:ERROR_MISSING_USERTOKEN_CODE userInfo:[NSDictionary dictionaryWithObject:ERROR_MISSING_USERTOKEN_MESSAGE forKey:NSLocalizedDescriptionKey]];
+        self.lastError = error;
+        
+        completionHandler(nil,error);
+        
+    }
+    
+    return FALSE;
+
+}
+
+
+/**
+ *  checkEmailAddress
+ *
+ *  Before runing BE communication
+ *  Checks for email address, API Key and compeltion handler
+ *
+ *  @param completionHandler completionHandler to check
+ *  @param emailAddress      email address
+ *
+ *  @return TRUE for passed Checks, FALSE for failed Checks
+ */
+
+- (BOOL) checkEmailAddress:(NSString*)emailAddress andCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
+    
+    if ([self checkAPIKeyWithCompletion:completionHandler]) {
+        
+        if (emailAddress != Nil) {
+            
+            if ([emailAddress isKindOfClass:[NSString class]]) {
+                
+                if(emailAddress.length > 0) {
+                    self.lastError = Nil;
+                    return TRUE;
+                }
+            }
+        }
+        
+        NSError* error = [NSError errorWithDomain:kInAppSDKErrorDomain code:ERROR_MISSING_EMAILADDRESS_CODE userInfo:[NSDictionary dictionaryWithObject:ERROR_MISSING_EMAILADDRESS_MESSAGE forKey:NSLocalizedDescriptionKey]];
+        self.lastError = error;
+        
+        completionHandler(nil,error);
+        
+    }
+
+    return FALSE;
+}
+
+
+
+/**
+ *  checkAPIKeyWithCompletion
+ *
+ *  Before runing BE communication
+ *  Checks for valid API Key and completion Handler
+ *
+ *  @param completionHandler completionHandler to check
+ *
+ *  @return TRUE for passed Checks, FALSE for failed Checks
+ */
+
+- (BOOL) checkAPIKeyWithCompletion:(PLVInAppAPIClientCompletionHandler)completionHandler {
+    
+    if (self.apiKey == Nil) {
+        
+        NSError* error = [NSError errorWithDomain:kInAppSDKErrorDomain code:ERROR_MISSING_API_KEY_CODE userInfo:[NSDictionary dictionaryWithObject:ERROR_MISSING_API_KEY_MESSAGE forKey:NSLocalizedDescriptionKey]];
+        self.lastError = error;
+        
+        if (completionHandler != Nil) {
+            completionHandler(nil,error);
+        }
+        
+        return FALSE;
+    }
+    
+    if (completionHandler == Nil) {
+        
+        NSError* error = [NSError errorWithDomain:kInAppSDKErrorDomain code:ERROR_MISSING_CALLBACK_CODE userInfo:[NSDictionary dictionaryWithObject:ERROR_MISSING_CALLBACK_MESSAGE forKey:NSLocalizedDescriptionKey]];
+        self.lastError = error;
+        
+        return FALSE;
+    }
+    
+    self.lastError = Nil;
+    
+    return TRUE;
+}
 
 @end
