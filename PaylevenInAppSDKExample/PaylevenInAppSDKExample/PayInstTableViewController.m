a7b9afb4c389eab498fe941ae0588c04b43bb3b1
Thomas Ploentzke <ploenne@ploenne.net>
2014-11-07 15:05:48 +0100

    addPIList Controller

--- PaylevenInAppSDKExample/PaylevenInAppSDKExample/PayInstTableViewController.m
+++ PaylevenInAppSDKExample/PaylevenInAppSDKExample/PayInstTableViewController.m
@@ -11,6 +11,20 @@
 
 #define piLiastTableViewCell @"piLiastTableViewCell"
 
+@interface PayInstrumentsTableCell : UITableViewCell
+
+@property (weak) IBOutlet UILabel* typeLabel;
+@property (weak) IBOutlet UILabel* mainLabel;
+@property (weak) IBOutlet UILabel* validLabel;
+
+@end
+
+@implementation PayInstrumentsTableCell
+
+
+
+@end
+
 @interface PayInstTableViewController ()
 
 @property (weak) IBOutlet UITableView* tableView;
@@ -26,7 +40,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:piLiastTableViewCell];
+    UINib* cellNib = [UINib nibWithNibName:@"PayInstrumentsTableCell" bundle:Nil];
+    
+    
+    [self.tableView registerNib:cellNib forCellReuseIdentifier:piLiastTableViewCell];
     
     [self.tableView reloadData];
 }
@@ -51,11 +68,13 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:piLiastTableViewCell forIndexPath:indexPath];
+    PayInstrumentsTableCell* cell = (PayInstrumentsTableCell*)[tableView dequeueReusableCellWithIdentifier:piLiastTableViewCell forIndexPath:indexPath];
     
     PLVPaymentInstrument* pi = [self.payInstruments objectAtIndex:indexPath.row];
     
-    cell.textLabel.text = pi.identifier;
+    cell.mainLabel.text = [self humanIdentifierForPI:pi];
+    cell.typeLabel.text = [self humanTypeForShort:pi.type];
+    cell.validLabel.text = [self humandValidForPI:pi];
     
     return cell;
 }
@@ -65,4 +84,68 @@
     
     return self.payInstruments.count;
 }
+
+- (NSString*) humanTypeForShort:(NSString*)shortType {
+    
+    if([shortType isEqualToString:@"CC"]) {
+        return @"CreditCard";
+    } else if([shortType isEqualToString:@"DD"]) {
+        return @"DebitCard";
+    } else if([shortType isEqualToString:@"SEPA"]) {
+        return @"SEPA Account";
+    } else if([shortType isEqualToString:@"PAYPAL"]) {
+        return @"PaylPal";
+    }  else {
+        return @"unknown";
+    }
+    
+}
+
+- (NSString*) humanIdentifierForPI:(PLVPaymentInstrument*)pi {
+    
+    NSString* shortType = pi.type;
+    
+    if([shortType isEqualToString:@"CC"]) {
+        PLVPayInstrumentCC* cc = (PLVPayInstrumentCC*)pi;
+        return cc.pan;
+    } else if([shortType isEqualToString:@"DD"]) {
+        PLVPayInstrumentDD* cc = (PLVPayInstrumentDD*)pi;
+        return cc.accountNumber;
+    } else if([shortType isEqualToString:@"SEPA"]) {
+        PLVPayInstrumentSEPA* cc = (PLVPayInstrumentSEPA*)pi;
+        return [NSString stringWithFormat:@"IBAN:%@",cc.iban];
+    } else if([shortType isEqualToString:@"PAYPAL"]) {
+        PLVPayInstrumentPAYPAL* cc = (PLVPayInstrumentPAYPAL*)pi;
+        return [NSString stringWithFormat:@"PAYPAYL:%@",cc.emailAddress];
+    }  else {
+        return @"unknown";
+    }
+    
+}
+
+- (NSString*) humandValidForPI:(PLVPaymentInstrument*)pi {
+    
+    NSString* shortType = pi.type;
+    
+    if([shortType isEqualToString:@"CC"]) {
+        PLVPayInstrumentCC* cc = (PLVPayInstrumentCC*)pi;
+        return [NSString stringWithFormat:@"%@/%@",cc.expiryMonth,cc.expiryYear];
+    } else if([shortType isEqualToString:@"DD"]) {
+        PLVPayInstrumentDD* cc = (PLVPayInstrumentDD*)pi;
+        return [NSString stringWithFormat:@"%@/%@",cc.expiryMonth,cc.expiryYear];
+    } else if([shortType isEqualToString:@"SEPA"]) {
+        PLVPayInstrumentSEPA* cc = (PLVPayInstrumentSEPA*)pi;
+        return [NSString stringWithFormat:@"%@/%@",cc.expiryMonth,cc.expiryYear];
+    } else if([shortType isEqualToString:@"PAYPAL"]) {
+
+        return @"";
+    }  else {
+        return @"unknown";
+    }
+    
+}
+
 @end
+
+
+
