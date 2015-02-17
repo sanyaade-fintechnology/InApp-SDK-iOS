274426bbc28fdc7adad63d93e690c36c68eab2e9
Thomas Ploentzke <ploenne@ploenne.net>
2015-02-17 16:48:46 +0100

    update buildscript

--- Scripts/BuildSampleAppForHockey.sh
+++ Scripts/BuildSampleAppForHockey.sh
@@ -15,11 +15,11 @@ rm -rf "$BUILD_ROOT"
 
 security unlock-keychain -p "builderpayleven" ~/Library/Keychains/login.keychain
 
-xcodebuild -workspace PaylevenInAppSDK.xcworkspace -scheme PaylevenInAppSDK -configuration Release -sdk iphoneos -destination 'generic/platform=iOS' archive -archivePath $BUILD_ROOT/$PRODUCT_NAME
+xcodebuild -workspace PaylevenInAppSDK.xcworkspace -scheme ExampleApp -configuration Release -sdk iphoneos -destination 'generic/platform=iOS' archive -archivePath $BUILD_ROOT/$PRODUCT_NAME
 
-xcodebuild -workspace PaylevenInAppSDK.xcworkspace -scheme BundleFramework -configuration Release -sdk iphoneos -destination 'generic/platform=iOS' archive -archivePath $BUILD_ROOT/$PRODUCT_NAME
+# xcodebuild -workspace PaylevenInAppSDK.xcworkspace -scheme BundleFramework -configuration Release -sdk iphoneos -destination 'generic/platform=iOS' archive -archivePath $BUILD_ROOT/$PRODUCT_NAME
 
-xcodebuild -workspace PaylevenInAppSDK.xcworkspace -scheme InAppSDKExample -configuration Release -sdk iphoneos -destination 'generic/platform=iOS' archive -archivePath $BUILD_ROOT/$PRODUCT_NAME
+# xcodebuild -workspace PaylevenInAppSDK.xcworkspace -scheme InAppSDKExample -configuration Release -sdk iphoneos -destination 'generic/platform=iOS' archive -archivePath $BUILD_ROOT/$PRODUCT_NAME
 
 xcodebuild -workspace PaylevenInAppSDK.xcworkspace -scheme BuildDocs -configuration Release -sdk iphoneos -destination 'generic/platform=iOS' archive -archivePath $BUILD_ROOT/$PRODUCT_NAME
 
