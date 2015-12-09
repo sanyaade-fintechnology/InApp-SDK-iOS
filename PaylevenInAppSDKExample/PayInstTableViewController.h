da68c2cd4eb2556e81495c88d207ba466aa91ca1
jenkins <jenkins@builds-Mac-mini.local>
2015-12-09 11:38:48 +0100

    Sample updated on 12-09-15

---
+++ PaylevenInAppSDKExample/PayInstTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  PayInstTableViewController.h
+//  PaylevenInAppSDKExample
+//
+//  Created by ploenne on 07.11.14.
+//  Copyright (c) 2014 payleven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayInstTableViewController : UIViewController
+
+@property (nonatomic, strong) NSString* userToken;
+@property (nonatomic, strong) NSString* useCase;
+
+@end
