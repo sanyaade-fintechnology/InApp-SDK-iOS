a4e3d079099f57e91a716390b14bfac974aff89b
jenkins <jenkins@builds-Mac-mini.local>
2015-12-10 11:52:21 +0100

    Sample updated on 12-10-15

---
+++ PaylevenInAppSDKExample/AddPIViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddPIViewController.h
+//  PaylevenInAppSDKExample
+//
+//  Created by ploenne on 10.11.14.
+//  Copyright (c) 2014 payleven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPIViewController : UIViewController
+
+@property (strong) NSString* userToken;
+@property (strong) NSString* emailAddress;
+
+//to create a User Token you must create a payment instrument first
+@property BOOL paymentInstrumentIsMandatory;
+
+@end
